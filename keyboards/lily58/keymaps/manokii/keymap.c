155df7d67af3d71c61e59e5350ea4c0026ddb5b3
Jasper Concepcion <jasper@manok.dev>
2022-07-17 05:24:58 +0800

    q

--- keyboards/lily58/keymaps/manokii/keymap.c
+++ keyboards/lily58/keymaps/manokii/keymap.c
@@ -47,32 +47,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_GRV,
-                        KC_LGUI,  KC_LALT,   LOWER,  KC_SPC,   KC_ENT,   KC_BSPC,  RAISE, RAISE
+  KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  RAISE,
+                        KC_LGUI,  KC_LALT,   LOWER,  KC_SPC,   KC_ENT,   KC_BSPC,  KC_LCBR, KC_RCBR
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |                    |      |      |      |   (  |   )  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |      |                    |   ^  |   &  |   *  |   (  |   )  |   =  |
+ * |      |  F1  |  F2  |  F3  |  F4  |      |                    |   ^  |   &  |  Up  |   {  |   }  |   =  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |  F5  |  F6  |  F7  |  F8  |      |-------.    ,-------|   6  |   7  |   8  |   {  |   }  |   >  |
+ * |      |  F5  |  F6  |  F7  |  F8  |      |-------.    ,-------|   6  | Left | Down |Right |      |   >  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |      |  F9  | F10  | F11  | F12  |      |-------|    |-------|   |  |   `  |   +  |   {  |   }   |  |   |
+ * |      |  F9  | F10  | F11  | F12  |      |-------|    |-------|   |  |   `  |   +  |   [  |   ]   |  |   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |LOWER | LGUI | Alt  | /Space  /       \Enter \  |Delete| RGUI |RAISE |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `-------------------''-------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, KC_LPRN, KC_RPRN, KC_SPC,
+  _______, _______, _______, _______, _______, _______,                   _______, KC_LPRN, KC_RPRN, KC_LPRN, KC_RPRN, KC_SPC,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,                   _______, KC_GRV, KC_UP, KC_LCBR, KC_RCBR, KC_EQL,
   _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,                   KC_GRV, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_GT,
-  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MPLY, KC_MNXT, KC_PIPE, KC_GRAVE, KC_PLUS, _______, _______, KC_PIPE,
-                             _______, _______, _______, _______, _______,  KC_DEL, _______, _______
+  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MPLY, KC_MNXT, KC_PIPE, KC_GRAVE, KC_PLUS, KC_LBRC, KC_RBRC, KC_PIPE,
+                             _______, _______, _______, _______, _______, _______, _______, _______
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
