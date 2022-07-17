082955a0a15dddef26acee6cf0e29784ecf87bd8
Jasper Concepcion <jasper@manok.dev>
2022-07-17 08:14:12 +0800

    q

--- keyboards/lily58/keymaps/manokii/keymap.c
+++ keyboards/lily58/keymaps/manokii/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  RAISE,
-                        KC_LGUI,  KC_LALT,   LOWER,  KC_SPC,   KC_ENT,   KC_BSPC,  KC_LCBR, KC_RCBR
+                        KC_LGUI,  KC_LALT,   LOWER,  KC_SPC,   KC_ENT,   KC_BSPC,  RAISE, RAISE
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -68,11 +68,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `-------------------''-------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, KC_LPRN, KC_RPRN, KC_LPRN, KC_RPRN, KC_SPC,
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,                   _______, KC_GRV, KC_UP, KC_LCBR, KC_RCBR, KC_EQL,
-  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,                   KC_GRV, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_GT,
-  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MPLY, KC_MNXT, KC_PIPE, KC_GRAVE, KC_PLUS, KC_LBRC, KC_RBRC, KC_PIPE,
-                             _______, _______, _______, _______, _______, _______, _______, _______
+  _______, _______, _______, _______, _______, _______,                   LC_PSCR, KC_LPRN, KC_RPRN, KC_LPRN, KC_RPRN, KC_SPC,
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,                   _______, KC_GRV, KC_UP, KC_LCBR, KC_RCBR, KC_GT,
+  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,                   KC_GRV, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PIPE,
+  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MPLY, KC_MNXT, KC_LCBR, KC_RCBR, KC_PLUS, KC_LBRC, KC_RBRC, KC_PIPE,
+                             _______, _______, _______, _______, _______, KC_DEL, _______, _______
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -91,8 +91,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______,                     _______, _______,   KC_UP, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______,                     XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,   XXXXXXX, XXXXXXX,
+  _______, KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     _______, _______,   KC_UP, _______, _______, _______,
+  _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                     XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,   XXXXXXX, XXXXXXX,
   _______, _______, _______, _______, _______, _______,  KC_MUTE, KC_MPRV,  KC_PLUS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, _______,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
